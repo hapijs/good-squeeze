09bdf2c0415dd4131cfa9427f1f69c71c1c13b9f
Max <max.nachlinger@walmart.com>
2017-03-02 08:16:50 -0800

    Added a few silly tests for buffers and strings.

--- test/index.js
+++ test/index.js
@@ -284,7 +284,7 @@ describe('SafeJson', () => {
         read.push(null);
     });
 
-    it('adds a seperator value when specified', { plan: 1 }, (done) => {
+    it('adds a separator value when specified', { plan: 1 }, (done) => {
 
         let result = '';
         const stream = new SafeJson({}, { separator: '#' });
@@ -305,6 +305,54 @@ describe('SafeJson', () => {
 
         read.push({ foo: 'bar' });
         read.push({ bar: 'baz' });
+        read.push(null);
+    });
+
+    it('serializes incoming buffer data', { plan: 1 }, (done) => {
+
+        let result = '';
+        const stream = new SafeJson({}, { separator: '#' });
+        const read = internals.readStream();
+
+        stream.on('data', (data) => {
+
+            result += data;
+        });
+
+        stream.on('end', () => {
+
+            expect(result).to.equal('{\"type\":\"Buffer\",\"data\":[116,101,115,116,45,48]}#{\"type\":\"Buffer\",\"data\":[116,101,115,116,45,49]}#');
+            done();
+        });
+
+        read.pipe(stream);
+
+        read.push(new Buffer('test-0'));
+        read.push(new Buffer('test-1'));
+        read.push(null);
+    });
+
+    it('serializes incoming string data', { plan: 1 }, (done) => {
+
+        let result = '';
+        const stream = new SafeJson({}, { separator: '#' });
+        const read = internals.readStream();
+
+        stream.on('data', (data) => {
+
+            result += data;
+        });
+
+        stream.on('end', () => {
+
+            expect(result).to.equal('"test-0"#"test-1"#');
+            done();
+        });
+
+        read.pipe(stream);
+
+        read.push('test-0');
+        read.push('test-1');
         read.push(null);
     });
 });
